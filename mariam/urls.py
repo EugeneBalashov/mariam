97311f372bc16729e111f2f2b48d6e13e03d2b1f
Eugene Balashov <EugeneBalashov2012@gmail.com>
2013-09-15 22:37:45 +0300

    first commit

---
+++ mariam/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mariam.views.home', name='home'),
+    # url(r'^mariam/', include('mariam.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
